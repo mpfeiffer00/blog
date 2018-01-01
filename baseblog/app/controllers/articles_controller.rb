1c48a7913440eef4deb30380ac1ddb0908ab8f4d
mpfeiffer00 <mp.7069@gmail.com>
2018-01-01 17:21:08 +0000

    added functionality to edit articles

--- baseblog/app/controllers/articles_controller.rb
+++ baseblog/app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-    
+
   def show
       @article = Article.find(params[:id])
   end
@@ -15,6 +15,20 @@ class ArticlesController < ApplicationController
 
     @article.save
     redirect_to @article
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
