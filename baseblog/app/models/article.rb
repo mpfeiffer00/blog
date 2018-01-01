168db22fcc533de30abe140c91e785729a0bf6fb
mpfeiffer00 <mp.7069@gmail.com>
2018-01-01 02:52:09 +0000

    Added comment functionality on articles

--- baseblog/app/models/article.rb
+++ baseblog/app/models/article.rb
@@ -1,6 +1,7 @@
 class Article
   include Mongoid::Document
-   
+
+  has_many :comments, dependent: :delete
   field :title, type: String
   field :text, type: String
 end
---
+++ baseblog/app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment
+  include Mongoid::Document
+
+  belongs_to :article
+  field :commenter, type: String
+  field :body, type: String
+end
