812cb7979c8a30f311b5e5ace37b151ff3edff29
mpfeiffer00 <mp.7069@gmail.com>
2018-01-07 03:11:03 +0000

    Issue #4: Implemented categories on articles

--- baseblog/app/models/article.rb
+++ baseblog/app/models/article.rb
@@ -5,8 +5,17 @@ class Article
   field :title, type: String
   slug :title
   field :text, type: String
+  field :category, type: String
   has_many :comments, dependent: :delete
 
   validates_presence_of :title
   validates_presence_of :text
+  validates_presence_of :category
+
+  CATEGORY = [BLOG = "blog",
+    BAKING = "baking",
+    TEA = "tea",
+    DRINKS = "drinks",
+    COOKING = "cooking",
+    WHISKY = "whisky"]
 end
