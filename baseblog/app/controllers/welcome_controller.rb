0b3b5d0c3cccd78e4d35aec3e00781f4bf895dfa
mpfeiffer00 <mp.7069@gmail.com>
2018-01-15 02:43:28 +0000

    Updated tea section on index page

--- baseblog/app/controllers/welcome_controller.rb
+++ baseblog/app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    #@articles = Article.all
     @articles = Article.order_by(display_dttm: :desc).limit(5)
+    @tea_articles = Article.where(category: "tea").order_by(display_dttm: :desc).limit(4)
   end
 end
