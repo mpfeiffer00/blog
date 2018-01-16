f745b8a45fecd9e5d67082259841e89e14dd52bc
mpfeiffer00 <mp.7069@gmail.com>
2018-01-16 03:03:06 +0000

    Updated first section to be latest article of each category

--- baseblog/app/controllers/welcome_controller.rb
+++ baseblog/app/controllers/welcome_controller.rb
@@ -2,5 +2,16 @@ class WelcomeController < ApplicationController
   def index
     @articles = Article.order_by(display_dttm: :desc).limit(5)
     @tea_articles = Article.where(category: "tea").order_by(display_dttm: :desc).limit(4)
+
+    @latest_articles = []
+    @latest_articles << (Article.where(category: "blog").order_by(display_dttm: :desc).first)
+    @latest_articles << (Article.where(category: "baking").order_by(display_dttm: :desc).first)
+    @latest_articles << (Article.where(category: "tea").order_by(display_dttm: :desc).first)
+    @latest_articles << (Article.where(category: "drinks").order_by(display_dttm: :desc).first)
+    @latest_articles << (Article.where(category: "cooking").order_by(display_dttm: :desc).first)
+    @latest_articles << (Article.where(category: "whisky").order_by(display_dttm: :desc).first)
+    puts "latest: #{@latest_articles}"
+    @latest_articles = @latest_articles - [nil]
+    puts "latest: #{@latest_articles}"
   end
 end
